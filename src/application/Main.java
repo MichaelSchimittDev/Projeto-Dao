a12638d0d73d726975d0f6fc449ada0495830bef
Michael Starrk Schimitt <michaelschimittpro@gmail.com>
2023-10-24 18:41:55 -0300

    Metodo Update criado

--- src/application/Main.java
+++ src/application/Main.java
@@ -32,11 +32,18 @@ public class Main {
 			System.out.println(obj);
 		}
 		
+		//System.out.println();
+		//System.out.println("=== TEST 4: Seller insert ===");
+		//Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		//sellerDao.insert(newSeller);
+		//System.out.println("Insert! New id = " + newSeller.getId());
+		
 		System.out.println();
-		System.out.println("=== TEST 4: Seller insert ===");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-		sellerDao.insert(newSeller);
-		System.out.println("Insert! New id = " + newSeller.getId());
+		System.out.println("=== TEST 5: Seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
