560f5e6602a8372d6ade5c35e7fe1c333495895d
Michael Starrk Schimitt <michaelschimittpro@gmail.com>
2023-10-25 09:32:04 -0300

    Metodo Insert do DepartmentDaoJDBC feito

---
+++ src/application/Main2.java
@@ -0,0 +1,20 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("=== Test 1 Deparment Insert");
+		Department newDepartment = new Department(null, "Foods");
+		departmentDao.insert(newDepartment);
+		System.out.println("Insert is Done! New Id: " + newDepartment.getId());
+
+	}
+
+}
